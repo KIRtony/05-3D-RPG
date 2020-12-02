c1ffc44ff7604cd965f1ce074ab4f59d715671e6
KIRtony <ts15@iu.edu>
2020-12-02 15:41:59 +0700

    finish

---
+++ Player/UI/Win.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	var t_m = floor(Global.timer/60.0)
+	var t_s = Global.timer % 60
+	var t = ": %02d" % t_m
+	t += ":%02d" % t_s
+	$Label.text = "Victory!"
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Game.tscn")
+	Global.reset()
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
